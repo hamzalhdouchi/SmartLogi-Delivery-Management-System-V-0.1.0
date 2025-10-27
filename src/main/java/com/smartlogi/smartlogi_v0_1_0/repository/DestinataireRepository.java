13ad4c84076b7750304b1de5c1ca31390d490ef7
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-27 23:56:27 +0100

    feat : add DestinataireRepository for managing destinataire data with email and name search capabilities
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-19

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/repository/DestinataireRepository.java
@@ -0,0 +1,22 @@
+package com.smartlogi.smartlogi_v0_1_0.repository;
+
+
+import com.smartlogi.smartlogi_v0_1_0.entity.Destinataire;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DestinataireRepository extends JpaRepository<Destinataire, Long> {
+
+    Optional<Destinataire> findByEmail(String email);
+
+    List<Destinataire> findByNomContainingIgnoreCase(String nom);
+
+    @Query("SELECT d FROM Destinataire d WHERE d.nom LIKE %:keyword% OR d.prenom LIKE %:keyword% OR d.email LIKE %:keyword%")
+    List<Destinataire> searchByKeyword(@Param("keyword") String keyword);
+}
