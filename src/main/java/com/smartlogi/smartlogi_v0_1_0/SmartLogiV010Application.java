7a634ca93e45125b74c32aa9805b1db0e3bc7a76
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-27 11:48:29 +0100

    initialize project structure with Maven, add application properties, and configure .gitignore

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/SmartLogiV010Application.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smartlogi_v0_1_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogiV010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLogiV010Application.class, args);
+    }
+
+}
