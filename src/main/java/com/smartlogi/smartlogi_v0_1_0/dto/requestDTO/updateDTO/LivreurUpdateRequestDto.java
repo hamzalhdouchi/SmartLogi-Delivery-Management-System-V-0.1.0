279a1ff1c9069a96115965ef111fdd43dc352ca6
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:43:19 +0100

    feat : add LivreurUpdateRequestDto for updating delivery personnel information with validation
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/requestDTO/updateDTO/LivreurUpdateRequestDto.java
@@ -0,0 +1,34 @@
+package com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.updateDTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LivreurUpdateRequestDto {
+
+    @NotBlank(message = "L'ID est obligatoire")
+    private String id;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Size(max = 100, message = "Le prénom ne doit pas dépasser 100 caractères")
+    private String prenom;
+
+    @NotBlank(message = "Le téléphone est obligatoire")
+    @Size(max = 20, message = "Le téléphone ne doit pas dépasser 20 caractères")
+    private String telephone;
+
+    @NotBlank(message = "Le véhicule est obligatoire")
+    @Size(max = 50, message = "Le véhicule ne doit pas dépasser 50 caractères")
+    private String vehicule;
+
+    private String zoneId;
+}
