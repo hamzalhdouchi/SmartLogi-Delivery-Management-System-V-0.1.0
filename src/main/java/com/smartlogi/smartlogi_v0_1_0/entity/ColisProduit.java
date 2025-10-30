863350c24910dd8253ec3821ecdebef62c315015
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:56:02 +0100

    feat : enhance ColisProduit entity with validation and timestamp annotations
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-45

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/entity/ColisProduit.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/entity/ColisProduit.java
@@ -1,10 +1,15 @@
 package com.smartlogi.smartlogi_v0_1_0.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,25 +21,29 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ColisProduit {
+
     @EmbeddedId
     private ColisProduitId id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("colisId")
     @JoinColumn(name = "colis_id")
     private Colis colis;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("produitId")
     @JoinColumn(name = "produit_id")
     private Produit produit;
 
+
+    @Column(nullable = false)
     private Integer quantite;
 
+    @Column(precision = 10, scale = 2, nullable = false)
     private BigDecimal prix;
 
+    @CreationTimestamp
+    @Column(name = "date_ajout", updatable = false)
+    @PastOrPresent(message = "La date d'ajout ne peut pas être dans le futur")
     private LocalDateTime dateAjout;
-
-
 }
-
