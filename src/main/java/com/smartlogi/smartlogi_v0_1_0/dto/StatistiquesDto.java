58b5368c489aaa83760660fc9251b593fa2a920b
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:41:55 +0100

    feat : refactor StatistiquesDto to use Lombok for boilerplate code reduction
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/StatistiquesDto.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/StatistiquesDto.java
@@ -1,9 +1,17 @@
 package com.smartlogi.smartlogi_v0_1_0.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StatistiquesDto {
     private Long totalColis;
     private Long colisLives;
@@ -16,35 +24,5 @@ public class StatistiquesDto {
 
     private LocalDateTime dateGeneration;
 
-    // Constructeurs, Getters et Setters
-    public StatistiquesDto() {
-        this.dateGeneration = LocalDateTime.now();
-    }
 
-    public Long getTotalColis() { return totalColis; }
-    public void setTotalColis(Long totalColis) { this.totalColis = totalColis; }
-
-    public Long getColisLives() { return colisLives; }
-    public void setColisLives(Long colisLives) { this.colisLives = colisLives; }
-
-    public Long getColisEnCours() { return colisEnCours; }
-    public void setColisEnCours(Long colisEnCours) { this.colisEnCours = colisEnCours; }
-
-    public Map<String, Long> getColisParStatut() { return colisParStatut; }
-    public void setColisParStatut(Map<String, Long> colisParStatut) { this.colisParStatut = colisParStatut; }
-
-    public Map<String, Long> getColisParZone() { return colisParZone; }
-    public void setColisParZone(Map<String, Long> colisParZone) { this.colisParZone = colisParZone; }
-
-    public Map<String, Long> getColisParPriorite() { return colisParPriorite; }
-    public void setColisParPriorite(Map<String, Long> colisParPriorite) { this.colisParPriorite = colisParPriorite; }
-
-    public BigDecimal getPoidsTotal() { return poidsTotal; }
-    public void setPoidsTotal(BigDecimal poidsTotal) { this.poidsTotal = poidsTotal; }
-
-    public Long getNombreLivreursActifs() { return nombreLivreursActifs; }
-    public void setNombreLivreursActifs(Long nombreLivreursActifs) { this.nombreLivreursActifs = nombreLivreursActifs; }
-
-    public LocalDateTime getDateGeneration() { return dateGeneration; }
-    public void setDateGeneration(LocalDateTime dateGeneration) { this.dateGeneration = dateGeneration; }
 }
