56babf5ad7aef21759409d4325575943bd794075
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:52:24 +0100

    feat : add DestinataireCreateDto for creating recipient details with validation https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/requestDTO/createDTO/DestinataireCreateDto.java
@@ -0,0 +1,33 @@
+package com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.createDTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DestinataireCreateDto {
+    @NotBlank(message = "Le nom est obligatoire")
+    @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Size(max = 100, message = "Le prénom ne doit pas dépasser 100 caractères")
+    private String prenom;
+
+    @Email(message = "L'email doit être valide")
+    private String email;
+
+    @NotBlank(message = "Le téléphone est obligatoire")
+    @Size(max = 20, message = "Le téléphone ne doit pas dépasser 20 caractères")
+    private String telephone;
+
+    @NotBlank(message = "L'adresse est obligatoire")
+    private String adresse;
+
+}
