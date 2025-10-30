de6aee3f669f96a1069a56aefc9dd679895976fb
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:40:35 +0100

    feat : change id type in ZoneDto from Long to String https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/ZoneDto.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/ZoneDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public class ZoneDto {
-    private Long id;
+    private String id;
 
     @NotBlank(message = "Le nom est obligatoire")
     @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
@@ -21,24 +21,5 @@ public class ZoneDto {
 
     private LocalDateTime dateCreation;
 
-    // Constructeurs, Getters et Setters
-    public ZoneDto() {}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-
-    public String getCodePostal() { return codePostal; }
-    public void setCodePostal(String codePostal) { this.codePostal = codePostal; }
-
-    public Integer getNombreLivreurs() { return nombreLivreurs; }
-    public void setNombreLivreurs(Integer nombreLivreurs) { this.nombreLivreurs = nombreLivreurs; }
-
-    public Integer getNombreColis() { return nombreColis; }
-    public void setNombreColis(Integer nombreColis) { this.nombreColis = nombreColis; }
-
-    public LocalDateTime getDateCreation() { return dateCreation; }
-    public void setDateCreation(LocalDateTime dateCreation) { this.dateCreation = dateCreation; }
 }
