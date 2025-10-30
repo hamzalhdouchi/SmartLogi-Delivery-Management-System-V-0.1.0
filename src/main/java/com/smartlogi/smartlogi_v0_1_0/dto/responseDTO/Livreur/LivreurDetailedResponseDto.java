85493494558ca9f38401b2ea7cde917b0cbea85e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:43:45 +0100

    feat : add LivreurDetailedResponseDto for representing detailed delivery personnel information in responses
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/responseDTO/Livreur/LivreurDetailedResponseDto.java
@@ -0,0 +1,28 @@
+package com.smartlogi.smartlogi_v0_1_0.dto.responseDTO.Livreur;
+
+import com.smartlogi.smartlogi_v0_1_0.dto.responseDTO.Colis.ColisSimpleResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LivreurDetailedResponseDto {
+    private String id;
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String vehicule;
+    private LocalDateTime dateCreation;
+    private String zoneId;
+    private String zoneNom;
+    private Integer nombreColisAssignes;
+    private Integer nombreColisLives;
+    private Integer nombreColisEnCours;
+    private List<ColisSimpleResponseDto> colisAssignes = new ArrayList<>();
+}
