74339e0a74a72797d29ea2f0aac31d865958e0e2
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:47:06 +0100

    feat : add ColisProduitUpdateRequestDto for updating product entries with validation
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/requestDTO/updateDTO/ColisProduitUpdateRequestDto.java
@@ -0,0 +1,28 @@
+package com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.updateDTO;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ColisProduitUpdateRequestDto {
+
+    @NotBlank(message = "L'ID est obligatoire")
+    private String id;
+
+    @NotNull(message = "La quantité est obligatoire")
+    @Min(value = 1, message = "La quantité doit être au moins 1")
+    private Integer quantite;
+
+    @NotNull(message = "Le prix est obligatoire")
+    @Min(value = 0, message = "Le prix ne peut pas être négatif")
+    private BigDecimal prix;
+}
