d6bfef0129f1a28aef4fc9602756bbfaf6fd205a
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 00:44:19 +0100

    feat : add HistoriqueLivraisonUpdateRequestDto for updating delivery history information with validation
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-13

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/requestDTO/updateDTO/HistoriqueLivraisonUpdateRequestDto.java
@@ -0,0 +1,27 @@
+package com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.updateDTO;
+
+import com.smartlogi.smartlogi_v0_1_0.enums.StatutColis;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HistoriqueLivraisonUpdateRequestDto {
+
+    @NotBlank(message = "L'ID est obligatoire")
+    private String id;
+
+    @NotNull(message = "Le statut est obligatoire")
+    private StatutColis statut;
+
+    @NotNull(message = "La date de changement est obligatoire")
+    private LocalDateTime dateChangement;
+
+    private String commentaire;
+}
