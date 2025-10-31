23478d327d3378aa880cbe4f8ec360a76fb5255d
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-31 01:00:07 +0100

    feat : add DestinataireService interface for managing recipient operations
    https://hamzaalhadouchi-1735.atlassian.net/browse/SS-47

---
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/service/interfaces/DestinataireService.java
@@ -0,0 +1,21 @@
+package com.smartlogi.smartlogi_v0_1_0.service.interfaces;
+
+import com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.createDTO.DestinataireCreateDto;
+import com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.updateDTO.DestinataireUpdateDto;
+import com.smartlogi.smartlogi_v0_1_0.dto.responseDTO.Destinataire.DestinataireAdvancedResponseDto;
+import com.smartlogi.smartlogi_v0_1_0.dto.responseDTO.Destinataire.DestinataireSimpleResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface DestinataireService {
+    DestinataireSimpleResponseDto create(DestinataireCreateDto requestDto);
+    DestinataireSimpleResponseDto update(String id, DestinataireUpdateDto requestDto);
+    DestinataireSimpleResponseDto getById(String id);
+    DestinataireAdvancedResponseDto getByIdWithColis(String id);
+    Page<DestinataireSimpleResponseDto> getAll(Pageable pageable);
+    List<DestinataireSimpleResponseDto> searchByNom(String nom);
+    void delete(String id);
+    boolean existsById(String id);
+}
