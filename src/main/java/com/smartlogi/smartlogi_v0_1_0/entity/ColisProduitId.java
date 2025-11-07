7908473eab97b0ebc6d6bf466b44b82d1adbea4a
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-07 15:12:18 +0100

    SS-45 fix: enhance ColisProduitId with Lombok annotations and override equals/hashCode methods

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/entity/ColisProduitId.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/entity/ColisProduitId.java
@@ -1,11 +1,33 @@
 package com.smartlogi.smartlogi_v0_1_0.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
-class ColisProduitId implements Serializable {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ColisProduitId implements Serializable {
     private String colisId;
     private String produitId;
+
+    // ✅ Important: Override equals() and hashCode()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ColisProduitId that = (ColisProduitId) o;
+        return Objects.equals(colisId, that.colisId) &&
+                Objects.equals(produitId, that.produitId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(colisId, produitId);
+    }
 }
