ddebdc6192d46c75f73489499a55056bb921434e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-07 15:14:02 +0100

    SS-45 fix: remove validation annotations from ColisUpdateRequestDto fields

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/requestDTO/updateDTO/ColisUpdateRequestDto.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/dto/requestDTO/updateDTO/ColisUpdateRequestDto.java
@@ -17,20 +17,16 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ColisUpdateRequestDto {
 
-    @NotBlank(message = "The ID est obligatoire")
     private String id;
 
-    @NotBlank(message = "La description est obligatoire")
     private String description;
 
-    @NotNull(message = "Le poids est obligatoire")
     @DecimalMin(value = "0.1", message = "Le poids doit être supérieur à 0")
     private BigDecimal poids;
 
     private StatutColis statut;
     private Priorite priorite;
 
-    @NotBlank(message = "La ville de destination est obligatoire")
     @Size(max = 100, message = "La ville ne doit pas dépasser 100 caractères")
     private String villeDestination;
 
