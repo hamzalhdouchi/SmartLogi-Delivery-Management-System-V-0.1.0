2df376cd3d9fabbd73bc105601bad7207868df52
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-07 15:05:11 +0100

    SS-57 fix: change EmailAlreadyExistsException to extend RuntimeException and update error message

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/exception/EmailAlreadyExistsException.java
@@ -1,8 +1,12 @@
 package com.smartlogi.smartlogi_v0_1_0.exception;
 
-public class EmailAlreadyExistsException extends Exception {
+public class EmailAlreadyExistsException extends RuntimeException {
 
     public EmailAlreadyExistsException(String email) {
-        super("Cette email déga existe " + email);
+        super("L'email '" + email + "' est déjà utilisé par un autre destinataire");
+    }
+
+    public EmailAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
