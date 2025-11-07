d91f7699e9437a34e03545419b4e6cf7633514de
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-07 15:07:05 +0100

    SS-30 feat: rename getByIdWithColis to findByKeyWord in ClientExpediteurInterface

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/service/interfaces/clientExpsditeurInterface.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/service/interfaces/clientExpsditeurInterface.java
@@ -14,7 +14,7 @@ public interface clientExpsditeurInterface {
     ClientExpediteurSimpleResponseDto create(ClientExpediteurCreateRequestDto requestDto);
     ClientExpediteurSimpleResponseDto update(String id, ClientExpediteurUpdateRequestDto requestDto);
     ClientExpediteurSimpleResponseDto getById(String id);
-    ClientExpediteurAdvancedResponseDto getByIdWithColis(String id);
+    ClientExpediteurSimpleResponseDto findByKeyWord(String keyword);
     Page<ClientExpediteurSimpleResponseDto> getAll(Pageable pageable);
     List<ClientExpediteurSimpleResponseDto> searchByNom(String nom);
     void delete(String id);
