01606d4824f57bc871bc941b9cd8c09c2b47dc87
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-07 15:08:17 +0100

    SS-37 fix: remove unnecessary validation annotations from Livreur fields

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/entity/Livreur.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/entity/Livreur.java
@@ -27,33 +27,17 @@ public class Livreur {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @NotBlank(message = "Le nom du livreur est obligatoire")
-    @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
-    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s\\-']+$",
-            message = "Le nom ne doit contenir que des lettres, espaces, tirets et apostrophes")
     private String nom;
 
-    @NotBlank(message = "Le prénom du livreur est obligatoire")
-    @Size(max = 100, message = "Le prénom ne doit pas dépasser 100 caractères")
-    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s\\-']+$",
-            message = "Le prénom ne doit contenir que des lettres, espaces, tirets et apostrophes")
     private String prenom;
 
-    @NotBlank(message = "Le téléphone du livreur est obligatoire")
-    @Size(max = 20, message = "Le téléphone ne doit pas dépasser 20 caractères")
-    @Pattern(regexp = "^(\\+212|0)[5-7][0-9]{8}$",
-            message = "Le numéro de téléphone doit être un numéro marocain valide (ex: +212612345678 ou 0612345678)")
     private String telephone;
 
-    @NotBlank(message = "Le type de véhicule est obligatoire")
-    @Size(max = 50, message = "Le type de véhicule ne doit pas dépasser 50 caractères")
-    @Pattern(regexp = "^[a-zA-Z0-9À-ÿ\\s\\-]+$",
-            message = "Le type de véhicule contient des caractères non autorisés")
     private String vehicule;
 
     @CreationTimestamp
     @Column(name = "date_creation", updatable = false)
-    private LocalDateTime dateCreation;
+    private LocalDateTime dateCreation = LocalDateTime.now();
 
     @NotNull(message = "La zone assignée au livreur est obligatoire")
     @ManyToOne(fetch = FetchType.LAZY)
