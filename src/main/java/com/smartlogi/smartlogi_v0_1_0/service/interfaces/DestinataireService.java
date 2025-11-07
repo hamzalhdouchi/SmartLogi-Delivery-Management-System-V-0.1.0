65a510f54809bc7894ab76a56ed6f7a087ff29a0
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-07 14:43:05 +0100

    SS-31 feat : update create method in DestinataireService to throw EmailAlreadyExistsException and add findByKeyWord method

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/service/interfaces/DestinataireService.java
+++ src/main/java/com/smartlogi/smartlogi_v0_1_0/service/interfaces/DestinataireService.java
@@ -4,16 +4,18 @@ import com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.createDTO.DestinataireCreat
 import com.smartlogi.smartlogi_v0_1_0.dto.requestDTO.updateDTO.DestinataireUpdateDto;
 import com.smartlogi.smartlogi_v0_1_0.dto.responseDTO.Destinataire.DestinataireAdvancedResponseDto;
 import com.smartlogi.smartlogi_v0_1_0.dto.responseDTO.Destinataire.DestinataireSimpleResponseDto;
+import com.smartlogi.smartlogi_v0_1_0.exception.EmailAlreadyExistsException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface DestinataireService {
-    DestinataireSimpleResponseDto create(DestinataireCreateDto requestDto);
+    DestinataireSimpleResponseDto create(DestinataireCreateDto requestDto) throws EmailAlreadyExistsException;
     DestinataireSimpleResponseDto update(String id, DestinataireUpdateDto requestDto);
     DestinataireSimpleResponseDto getById(String id);
-    DestinataireAdvancedResponseDto getByIdWithColis(String id);
+//    DestinataireAdvancedResponseDto getByIdWithColis(String id);
+    DestinataireSimpleResponseDto findByKeyWord(String keyword);
     Page<DestinataireSimpleResponseDto> getAll(Pageable pageable);
     List<DestinataireSimpleResponseDto> searchByNom(String nom);
     void delete(String id);
