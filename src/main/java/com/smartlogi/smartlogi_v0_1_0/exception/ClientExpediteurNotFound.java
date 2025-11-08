a18cd6be33a317eb7185e4451a5c8ca15de8b123
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-08 21:59:53 +0100

    refactor: rename ClientExpediteurNotFound to NotFoundException and generalize constructor message

--- src/main/java/com/smartlogi/smartlogi_v0_1_0/exception/ClientExpediteurNotFound.java
+++
@@ -1,8 +0,0 @@
-package com.smartlogi.smartlogi_v0_1_0.exception;
-
-public class ClientExpediteurNotFound extends  RuntimeException{
-
-    public ClientExpediteurNotFound(String id) {
-        super("client expediteur has this id : "+ id +" not found");
-    }
-}
