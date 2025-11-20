e00426d29c25b2c0250c8149907d85565623d2e0
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-20 14:35:08 +0100

    feat: create User entity implementing UserDetails for authentication

---
+++ src/main/java/com/smartlogi/smartlogiv010/entity/User.java
@@ -0,0 +1,75 @@
+package com.smartlogi.smartlogiv010.entity;
+
+import com.smartlogi.smartlogiv010.enums.RoleUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String telephone;
+
+    @Column(unique = true)
+    private String email;
+
+    private String adresse;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RoleUser role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // Utiliser l'email comme username
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
