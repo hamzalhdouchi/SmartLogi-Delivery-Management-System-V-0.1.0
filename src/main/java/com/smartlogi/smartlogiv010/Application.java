3c13f4d529fca9269af64fec5ae67896e4614489
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:37:45 +0100

    feat: add startup message to indicate successful server initialization

--- src/main/java/com/smartlogi/smartlogiv010/Application.java
+++ src/main/java/com/smartlogi/smartlogiv010/Application.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+
+        System.out.println("||========================================||");
+        System.out.println("||      Server is working successfully    ||");
+        System.out.println("||========================================||");
     }
 
 
