ea3368df0b6ebc209112f07239110335e09bad12
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:50:40 +0100

    feat: add UserMapper for mapping User entities to UserResponse DTOs

---
+++ src/main/java/com/smartlogi/smartlogiv010/security/userMapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.smartlogi.smartlogiv010.security.userMapper;
+
+import com.smartlogi.smartlogiv010.entity.User;
+import com.smartlogi.smartlogiv010.security.dto.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = {RoleMapper.class}
+)
+public interface UserMapper {
+
+    UserResponse toResponse(User user);
+
+    List<UserResponse> toRoleResponseList(List<User> users);
+
+    @Mapping(target = "password", ignore = true) // Ne pas mapper le mot de passe
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
+    @Mapping(target = "accountNonExpired", ignore = true)
+    @Mapping(target = "accountNonLocked", ignore = true)
+    @Mapping(target = "credentialsNonExpired", ignore = true)
+    User toEntity(UserResponse userRoleResponse);
+}
