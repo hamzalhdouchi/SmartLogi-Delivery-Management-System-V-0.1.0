639e7aa38efd32ea71b187e6a531d9b9e4748ac5
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:39:30 +0100

    feat: add CheckRoleResponse DTO for role validation response

---
+++ src/main/java/com/smartlogi/smartlogiv010/security/dto/CheckRoleResponse.java
@@ -0,0 +1,10 @@
+package com.smartlogi.smartlogiv010.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CheckRoleResponse {
+    private boolean hasRole;
+}
