d50afb6e04ca6b0e859b412e6196ab045e37201d
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:42:02 +0100

    feat: rename AuthResponse to JwtAuthResponse and add token type

--- src/main/java/com/smartlogi/smartlogiv010/security/dto/JwtAuthResponse.java
+++ src/main/java/com/smartlogi/smartlogiv010/security/dto/JwtAuthResponse.java
@@ -2,11 +2,10 @@ package com.smartlogi.smartlogiv010.security.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-public class AuthResponse {
+public class JwtAuthResponse {
     private String token;
+    private String type = "Bearer";
 }
