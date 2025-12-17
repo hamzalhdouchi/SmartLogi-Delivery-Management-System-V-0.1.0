cc2ff71da0b2bc91eca1ae198c83e996e5aa3237
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:48:30 +0100

    feat: add UpdatePermissionRequest DTO for permission update data transfer

---
+++ src/main/java/com/smartlogi/smartlogiv010/security/dto/UpdatePermissionRequest.java
@@ -0,0 +1,9 @@
+package com.smartlogi.smartlogiv010.security.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdatePermissionRequest {
+    private String name;
+    private String description;
+}
