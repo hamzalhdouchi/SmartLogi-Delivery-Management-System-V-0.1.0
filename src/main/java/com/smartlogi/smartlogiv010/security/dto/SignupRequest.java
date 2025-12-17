955c40575c27282727d44d4cdd13b9080c4f6d49
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:47:33 +0100

    feat: add SignupRequest DTO for user registration data transfer

---
+++ src/main/java/com/smartlogi/smartlogiv010/security/dto/SignupRequest.java
@@ -0,0 +1,53 @@
+package com.smartlogi.smartlogiv010.security.dto;
+
+import com.smartlogi.smartlogiv010.enums.RoleUser;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+
+    @NotBlank(message = "Le nom est obligatoire")
+    @Size(min = 2, max = 50, message = "Le nom doit contenir entre 2 et 50 caractères")
+    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s'-]+$", message = "Le nom ne doit contenir que des lettres")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Size(min = 2, max = 50, message = "Le prénom doit contenir entre 2 et 50 caractères")
+    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s'-]+$", message = "Le prénom ne doit contenir que des lettres")
+    private String prenom;
+
+    @NotBlank(message = "Le téléphone est obligatoire")
+    @Pattern(
+            regexp = "^(\\+212|0)[5-7][0-9]{8}$",
+            message = "Format téléphone invalide. Utilisez: 0612345678 ou +212612345678"
+    )
+    private String telephone;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format email invalide")
+    @Size(max = 100, message = "L'email ne doit pas dépasser 100 caractères")
+    private String email;
+
+    @NotBlank(message = "L'adresse est obligatoire")
+    @Size(min = 10, max = 255, message = "L'adresse doit contenir entre 10 et 255 caractères")
+    private String adresse;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 8, max = 100, message = "Le mot de passe doit contenir entre 8 et 100 caractères")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "Le mot de passe doit contenir au moins: 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial"
+    )
+    private String password;
+
+    @NotNull(message = "Le rôle est obligatoire")
+    private String role;
+
+}
