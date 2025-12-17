4ba23e50d9eb1c020284187f599f9ccf7c6e12bd
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:51:05 +0100

    feat: update UserRepository to support role-based queries and change User ID type to String

--- src/main/java/com/smartlogi/smartlogiv010/repository/UserRepository.java
+++ src/main/java/com/smartlogi/smartlogiv010/repository/UserRepository.java
@@ -1,14 +1,20 @@
 package com.smartlogi.smartlogiv010.repository;
 
+import com.smartlogi.smartlogiv010.entity.Role;
 import com.smartlogi.smartlogiv010.entity.User;
+import com.smartlogi.smartlogiv010.enums.RoleUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
-
-public interface UserRepository extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 
+    boolean existsByRole(Role role);
+    long countByRole(Role role);
     boolean existsByEmail(String email);
+    Optional<User> findByRole(Role role);
 }
-
