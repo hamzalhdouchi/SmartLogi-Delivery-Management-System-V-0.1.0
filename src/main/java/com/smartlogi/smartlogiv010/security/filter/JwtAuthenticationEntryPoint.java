84ba6b828f88305a729c2e7415c36f75e86c4f7c
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-17 21:41:44 +0100

    feat: add JwtAuthenticationEntryPoint to handle unauthorized access

---
+++ src/main/java/com/smartlogi/smartlogiv010/security/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.smartlogi.smartlogiv010.security.filter;
+
+import jakarta.servlet.http.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\": \"Unauthorized - " +
+                authException.getMessage() + "\"}");
+    }
+}
+
