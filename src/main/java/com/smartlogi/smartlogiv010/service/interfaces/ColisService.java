4eb6eabd80794aee2ea53ddcb0ea1347497811d0
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-19 00:05:05 +0100

    feat: remove unused methods from ColisService interface

--- src/main/java/com/smartlogi/smartlogiv010/service/interfaces/ColisService.java
+++ src/main/java/com/smartlogi/smartlogiv010/service/interfaces/ColisService.java
@@ -36,7 +36,6 @@ public interface ColisService {
     List<ColisSimpleResponseDto> searchByVilleDestination(String ville);
     List<ColisSimpleResponseDto> getByPriorite(Priorite priorite);
     List<ColisSimpleResponseDto> getByPrioriteAndStatut(Priorite priorite, StatutColis statut);
-    List<ColisSimpleResponseDto> getByLivreurAndStatut(String livreurId, StatutColis statut);
     List<ColisSimpleResponseDto> getByZoneAndStatut(String zoneId, StatutColis statut);
     List<ColisSimpleResponseDto> getByVilleDestinationAndStatut(String ville, StatutColis statut);
     List<ColisProduit> getProduitsByColis(String colisId);
@@ -44,11 +43,9 @@ public interface ColisService {
     void mettreAJourQuantiteProduit(String colisId, String produitId, Integer nouvelleQuantite);
     void supprimerProduit(String colisId, String produitId);
     List<ColisSimpleResponseDto> searchByKeyword(String keyword);
-    List<ColisSimpleResponseDto> getByDateCreationBetween(LocalDateTime startDate, LocalDateTime endDate);
     List<ColisSimpleResponseDto> getColisEnRetard();
     Page<ColisSimpleResponseDto> getByZoneId(String zoneId, Pageable pageable);
     Page<ColisSimpleResponseDto> getByStatut(StatutColis statut, Pageable pageable);
-    List<PoidsParLivreurDTO> getPoidsTotalParLivreur();
     List<PoidsParLivreurDetailDTO> getPoidsDetailParLivreur();
     long countByStatut(StatutColis statut);
     long countByZoneAndStatut(String zoneId, StatutColis statut);
