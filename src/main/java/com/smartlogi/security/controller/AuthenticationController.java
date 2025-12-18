bebe6e9c3f27a5708d24d6808ec75398278ff44d
hamzalhadouchi <hamzabniss16@gmail.com>
2025-12-19 00:03:56 +0100

    feat: refactor AuthenticationController package structure and update response handling

---
+++ src/main/java/com/smartlogi/security/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package com.smartlogi.security.controller;
+
+import com.smartlogi.smartlogiv010.apiResponse.ApiResponse;
+import com.smartlogi.smartlogiv010.entity.User;
+import com.smartlogi.security.dto.authDto.response.JwtAuthResponse;
+import com.smartlogi.security.dto.authDto.request.LoginRequest;
+import com.smartlogi.security.dto.authDto.request.SignupRequest;
+import com.smartlogi.security.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @Autowired
+    private final AuthenticationService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<User>> signup(@Valid @RequestBody SignupRequest request) {
+        User user = authService.signup(request);
+        ApiResponse<User> response = ApiResponse.<User>builder()
+                .success(true)
+                .message("User récupérer avec succès")
+                .data(user)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+}
+
